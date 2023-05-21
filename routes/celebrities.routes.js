86895f749e194e43bf2f7b81e7a7ccc3b987d27c
Erika Balog <enbalog@gmail.com>
2023-05-21 14:27:15 +0200

    Updated lab

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,48 +3,79 @@ const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+
+//GET celebrities create
 router.get("/celebrities/create", (req, res, next) => {
-    res.render("./celebrities/new-celebrity");
-  });
+  res.render("./celebrities/new-celebrity");
+});
 
-  router.post("/celebrities/create", (req, res, next) => {
-    console.log('req body pls', req.body)
-    const { name, occupation, catchPhrase } = req.body;
+//POST celebrities create
+router.post("/celebrities/create", (req, res, next) => {
+  console.log("req body pls", req.body);
+  const { name, occupation, catchPhrase } = req.body;
 
-    Celebrity.create({ name, occupation, catchPhrase })
-    .then(response => {
-        console.log('response create celebirty', response)
-
-        res.redirect("/celebrities")
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then((response) => {
+      //Celebrity from DB
+      console.log("response create celebirty", response);
+      res.redirect("/celebrities");
     })
-    .catch(err => {
-        console.log(err)
+    .catch((err) => {
+      console.log(err);
+      res.render("celebrities/new-celebrity.hbs");
+    });
+  res.render("celebrities/new-celebrity");
+});
 
-        res.render("celebrities/new-celebrity.hbs");
-
-    }
-
-        )
-    res.render("celebrities/new-celebrity");
-  });
-
-  router.get("/celebrities", (req, res, next) => {
-    Celebrity.find() //buscamos em nuestra base de datos 
-    .then((results)=>{
-      const celebrityResults= {results} //transform array in obj
-      console.log(celebrityResults)
+//GET celebrities
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find() //retrive all celebrities
+    .then((results) => {
+      const celebrityResults = { results }; //transform array in obj
+      console.log(celebrityResults);
       return celebrityResults;
-
     })
-    .then(celebResults=>{
+    .then((celebResults) => {
       res.render("celebrities/celebrities", celebResults);
     })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-    .catch(err=> {
-      next(err)
+//GET /celebrities/:byId
+router.get("/celebrities/:id", (req, res, next) => {
+  Celebrity.findById(id)
+    .then((results) => {
+      res.render("celebritues/celebrities-detail.hbs", { celebritie: theCele });
     })
-  })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-   
+//POST /celebrities/findByIdAndUpdate
+router.post("/celebrities/:id/edit", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.findByIdAndUpdate(id, req.body)
+    .then((results) => {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+
+//POST delete celebrities
+router.post("/celebrities/:id/delete", (req, res, next) => {
+  Celebrity.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,23 +1,50 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-const Celebrity = require("../models/Celebrity.model");
+const Celebrity = require("../models/Movienpm run dev.model");
 
-const Movies= require("../models/Movies.model");
+const Movies = require("../models/Movies.model.js");
 
-router.get("/movies/create", (req, res, next)=>{
-    Celebrity.find()
-    .then(celebrityDb=>{
-        res.render("/movies/new-movie", {celebrityDb});
+//GET movies
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((allMovies) => {
+      res.render("movies/movies", { movies: allMovies });
     })
-    .catch(err=> next(err));
-})
+    .catch((error) => {
+      console.log("Error while getting the movies from the DB: ", error);
+      next(error);
+    });
+});
 
-router.post("/movies/create", (req,res,next)=> {
-    Movies.create(req.body)
-    .then(result=> {
-        res.redirect("/movies")
+//GET movie create
+router.get("/movies/create", (req, res, next) => {
+      res.render("/movies/new-movie");
+});
+
+//GET movie/:id
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(id)
+    .then((theMovie) => {
+      res.render("movies/movie-dettails.hbs", { movie: theMovie });
     })
-    .catch(err=> {
-        res.render("/movies/new-movie")
+    .catch((err) => next(err));
+});
+
+// //GET movie/:id/edit
+// router.get("/movies/:id/edit", (req, res, next)=>{
+//     Movie.findById(id)
+//     .then((movieEdit)=>{
+//         res.render("movies/edit-movie.hbs", {movie: movieEdit})
+//     })
+//     .catch((err) => next(err));
+// })
+
+//POST /movie/create
+router.post("/movies/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  Movies.create(req.body)
+    .then((result) => {
+      res.redirect("/movies");
     })
-})
+    .catch((err) => next(err));
+});
